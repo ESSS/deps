be6d0ed393eb80729e2148b9e4a7562a1f8aa776
Martin Prusse <prusse@esss.com.br>
2016-01-18 16:07:46 -0200

    Test for task execution

--- source/python/deps/_tests/test_deps_cli.py
+++ source/python/deps/_tests/test_deps_cli.py
@@ -1,11 +1,10 @@
 from __future__ import unicode_literals
-
-import textwrap
-
 from _pytest.pytester import LineMatcher
 from deps import deps_cli
 import os
 import pytest
+import stat
+import textwrap
 
 
 def test_deps_help(cli_runner):
@@ -139,6 +138,44 @@ def test_multiple_projects(cli_runner, project_tree):
     )
 
 
+def test_script_execution(cli_runner, project_tree, piped_shell_execute):
+    """
+    :type cli_runner: click.testing.CliRunner
+    :type project_tree: py.path.local
+    """
+    root_b = unicode(project_tree.join('root_b'))
+    command_args = ['-p', root_b, '-v', 'asd', '{name}', '{abs}']
+    result = cli_runner.invoke(deps_cli.cli, command_args)
+    print
+    print result.output
+    assert False
+    assert result.exit_code == 0
+    matcher = LineMatcher(result.output.splitlines())
+    matcher.fnmatch_lines([
+        '===============================================================================',
+        'dep_z:',
+        'deps: executing: asd dep_z *dep_z',
+        'deps: from:      *dep_z',
+        'Sample script dep_z *dep_z',
+        '',
+        'deps: return code: 0',
+        '',
+        '===============================================================================',
+        'dep_b.1.1: skipping',
+        '',
+        '===============================================================================',
+        'dep_b.1: skipping',
+        '',
+        '===============================================================================',
+        'root_b:',
+        'deps: executing: asd root_b *root_b',
+        'deps: from:      *root_b',
+        'Sample script root_b *root_b',
+        '',
+        'deps: return code: 0',
+    ])
+
+
 @pytest.fixture(scope='session')
 def project_tree(tmpdir_factory):
     """
@@ -167,5 +204,25 @@ def project_tree(tmpdir_factory):
                 ['  - {{{{ root }}}}/../{}/environment.yml'.format(dep) for dep in deps])
             env_content.append('')
         env_yml.write('\n'.join(env_content))
+    # Add test scripts to some projects.
+    batch_script = textwrap.dedent(
+        '''\
+        @echo Sample script %*
+        '''
+    )
+    bash_script = textwrap.dedent(
+        '''\
+        #!/bin/bash
+        echo Sample script "$@"
+        '''
+    )
+    for proj in ['root_b', 'dep_z']:
+        script_file = test_projects.join(proj).join('asd.bat')
+        script_file.write(batch_script)
+        script_file = test_projects.join(proj).join('asd')
+        script_file.write(bash_script)
+        script_file = unicode(script_file)
+        st = os.stat(script_file)
+        os.chmod(script_file, st.st_mode | stat.S_IEXEC)
     return test_projects
 
