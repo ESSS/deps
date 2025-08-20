db414a56569db8a43ce4fc414e1911b9d10e4412
Bruno Oliveira <bruno@esss.com>
2025-08-20 13:00:15 -0300

    Add full type annotations

--- src/deps/_synchronous_executor.py
+++ src/deps/_synchronous_executor.py
@@ -3,36 +3,39 @@ Helper module to create a synchronous executor to be used replacing the ThreadPo
 when running synchronously.
 """
 
+from __future__ import annotations
+from collections.abc import Callable
+from typing import Any
+
 
 class Future:
-    def __init__(self, callback, args):
+    def __init__(self, callback: Callable[..., Any], args: Any) -> None:
         try:
-            self._result = callback(*args)
-            self._exception = None
+            self._result: Any | None = callback(*args)
+            self._exception: Exception | None = None
         except Exception as err:
             self._result = None
             self._exception = err
-        self.callbacks = []
 
-    def cancelled(self):
+    def cancelled(self) -> bool:
         return False
 
-    def cancel(self):
+    def cancel(self) -> bool:
         return False
 
-    def done(self):
+    def done(self) -> bool:
         return True
 
-    def exception(self):
+    def exception(self) -> Exception | None:
         return self._exception
 
-    def result(self):
+    def result(self) -> Any:
         if self._exception is not None:
             raise self._exception
         else:
             return self._result
 
-    def add_done_callback(self, callback):
+    def add_done_callback(self, callback: Callable[[Future], Any]) -> None:
         callback(self)
 
 
@@ -41,8 +44,8 @@ class SynchronousExecutor:
     Synchronous executor: submit() blocks until it gets the result.
     """
 
-    def submit(self, callback, *args):
+    def submit(self, callback: Callable[..., Any], *args: Any) -> Future:
         return Future(callback, args)
 
-    def shutdown(self, wait):
+    def shutdown(self, wait: bool) -> None:
         pass
--- src/deps/fixtures.py
+++ src/deps/fixtures.py
@@ -1,9 +1,15 @@
+from collections.abc import Iterator
+from pathlib import Path
+from typing import Any
+from unittest.mock import MagicMock
+
 import pytest
 from click.testing import CliRunner
+from pytest_mock import MockerFixture
 
 
 @pytest.yield_fixture
-def cli_runner():
+def cli_runner() -> Iterator[CliRunner]:
     """
     Fixture used to test click applications.
     :rtype: click.testing.CliRunner
@@ -12,11 +18,13 @@ def cli_runner():
 
 
 @pytest.fixture
-def piped_shell_execute(mocker):
+def piped_shell_execute(mocker: MockerFixture) -> None:
     import click
     import subprocess
 
-    def _piped_shell_execute(command, cwd, buffer_output=False):
+    def _piped_shell_execute(
+        command: str, cwd: str | Path, buffer_output: bool = False
+    ) -> Any:
         # This version always makes the pipe regardless of the buffer_output value and always
         # redirects everything for stdout.
         process = subprocess.Popen(
@@ -24,15 +32,14 @@ def piped_shell_execute(mocker):
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             shell=True,
+            text=True,
             cwd=cwd,
         )
         stdout, stderr = process.communicate()
-        stdout = stdout.decode()
         click.secho(stdout)
         return process, stdout, stderr, 0
 
-    shell_execute = mocker.patch(
+    mocker.patch(
         "deps.deps_cli.shell_execute",
         new=_piped_shell_execute,
     )
-    return shell_execute
